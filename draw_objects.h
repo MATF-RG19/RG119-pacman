b281fbf8bb4d466d6d18884a3758d6085759aeba
wnsmith <eap846@gmail.com>
2020-05-12 15:11:26 +0200

    draw pacman

--- draw_objects.h
+++ draw_objects.h
@@ -1,5 +1,12 @@
 #include "constants.h"
 
+extern int position[2];
+extern int next_position[2];
+extern int wanted_direction[2];
+extern int game_timer;
+extern int rot_ply;
+extern float anim_param;
+
 
 // function to draw axis
 static void draw_line(int x, int y, int z, int length) {
@@ -34,4 +41,61 @@ static void draw_board() {
             if (board[i][j] == 2)
                 set_board_position(i, j);
     
+}
+
+
+static void draw_pacman(int rot)
+{   
+    int mouth_angle = (game_timer % 40);
+
+
+    glTranslatef(position[0],position[1],2);
+        
+        glRotatef(90+rot,0,0,1);
+        
+        glRotatef(-mouth_angle,0,1,0); 
+        
+        GLUquadric *newQuad = gluNewQuadric();
+        glColor3f(0.7, 0.6, 0);
+        gluDisk(newQuad,0,1.4,32,4);
+
+        glPushMatrix();
+            GLdouble plane0[] = {0,0,1,0};
+            glEnable(GL_CLIP_PLANE0);
+            glClipPlane(GL_CLIP_PLANE0, plane0);
+                glColor3f(0.8, 0.9, 0);
+                glutSolidSphere(1.5,20,20); 
+            glDisable(GL_CLIP_PLANE0);
+        glPopMatrix();
+        glPushMatrix();
+            glRotatef(2*mouth_angle,0,1,0);
+            glColor3f(0.7, 0.6, 0);
+            gluDisk(newQuad,0,1.4,32,4); 
+            GLdouble plane1[] = {0,0,-1,0};
+            glEnable(GL_CLIP_PLANE0);
+            glClipPlane(GL_CLIP_PLANE0, plane1);
+                glColor3f(0.8, 0.9, 0);
+                glutSolidSphere(1.5,20,20); 
+            glDisable(GL_CLIP_PLANE0);
+        glPopMatrix();
+        
+        // Eyes
+        glTranslatef(1.1,0.4,0.5);
+            glColor3f(1, 1, 1);
+            glutSolidSphere(.35,20,20);
+            glTranslatef(0.2,0,0.1);
+                glColor3f(0,0,0);
+                glutSolidSphere(.18,10,10);
+            glTranslatef(-0.2,0,-0.1);
+        glTranslatef(0,-0.8,0);
+            glColor3f(1, 1, 1);
+            glutSolidSphere(0.35,20,20);
+            glTranslatef(0.2,0,0.1);
+                glColor3f(0,0,0);
+                glutSolidSphere(.18,10,10);
+            glTranslatef(-0.2,0,-0.1);
+        glTranslatef(-1.1,0.4,-0.5);
+
+    glTranslatef(-position[0],-position[1],-2);
+
 }
--- gameplay.h
+++ gameplay.h
@@ -1,4 +1,5 @@
 #include "draw_objects.h"
+#include "stdio.h"
 
 
 #define POINT_SIZE 1
@@ -6,35 +7,37 @@
 
 
 /* look at point */
-#define EYEX 50
-#define EYEY -50
-#define EYEZ 50
+#define EYEX 60
+#define EYEY 30
+#define EYEZ 30
 
-#define CENTERX 0
-#define CENTERY 0
+#define CENTERX 30
+#define CENTERY 20
 #define CENTERZ 0
 
 #define UPX 0
 #define UPY 0
 #define UPZ 1
 
+
 extern int width, height;
 
-static void on_display(void) {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-    gluLookAt(EYEX, EYEY, EYEZ, CENTERX, CENTERY, CENTERZ, UPX, UPY, UPZ);
-    
-		draw_axis();
-    	draw_board();
+int position[2];
+int next_position[2];
+int wanted_direction[2];
+int game_timer;
+int rot_ply = 0;
+float anim_param;
 
-    glutPostRedisplay();
-    glutSwapBuffers();
-}
+static void on_keyboard(unsigned char key, int x, int y);
+static void on_display(void);
+static void on_reshape(int new_width, int new_height);
+static void init_game(void);
 
 
-static void on_keyboard(unsigned char key, int x, int y) {
+
+static void on_keyboard(unsigned char key, int x, int y)
+{
     switch (key) {
     case 27:   
         exit(0);
@@ -43,8 +46,25 @@ static void on_keyboard(unsigned char key, int x, int y) {
     glutPostRedisplay();
 }
 
-static void on_reshape(int new_width, int new_height)
-{
+static void on_display(void) {
+    game_timer++;
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(EYEX, EYEY, EYEZ, CENTERX, CENTERY, CENTERZ, UPX, UPY, UPZ);
+
+        
+		draw_axis();
+    	draw_board();
+        draw_pacman(rot_ply);
+
+    glutPostRedisplay();
+    glutSwapBuffers();
+}
+
+
+static void on_reshape(int new_width, int new_height) {
     width = new_width;
     height = new_height;
     glViewport(0, 0, new_width, new_height);
